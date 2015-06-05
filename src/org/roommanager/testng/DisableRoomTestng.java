ceec76536fae7b59742795018417120e242bb50a
Samuel Vargas <varguitas0911@gmail.com>
2015-06-05 16:16:35 -0400

    third commit of DisableRoom

---
+++ src/org/roommanager/testng/DisableRoomTestng.java
@@ -0,0 +1,83 @@
+package org.roommanager.testng;
+
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import org.testng.annotations.Test;
+
+public class DisableRoomTestng {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @BeforeTest
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://172.20.208.174:4042/";
+    driver.manage().window().maximize();
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test (priority=4)
+  public void testDisableARoom() throws Exception {
+    driver.get(baseUrl + "/admin/#/login");
+    driver.findElement(By.xpath("//button")).click();
+    driver.findElement(By.xpath("//button")).click();
+    driver.findElement(By.linkText("Conference Rooms")).click();
+    driver.findElement(By.xpath("//div[@id='roomsGrid']/div[2]/div/div[4]/div/div[2]/div/div/div/span")).click();
+    assertEquals("", driver.findElement(By.xpath("//div[@id='roomsGrid']/div[2]/div/div[2]/div/div[2]/div/div/div/span")).getText());
+  }
+
+  @AfterTest
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
